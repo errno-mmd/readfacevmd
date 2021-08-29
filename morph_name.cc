e792c2cf27ee8a79135f27ea437d3f58a31fb8e8
errno-mmd <errno.mmd2@gmail.com>
2021-08-29 20:21:58 +0900

    omit needless morph/bone

--- morph_name.cc
+++ morph_name.cc
@@ -32,13 +32,19 @@ map<string, string> make_rename_map(string fname_conf)
 
 void rename_morph(VMD& v, const map<string, string>& rename_map)
 {
-  for (VMD_Morph& m : v.morph) {
+  for (auto it = v.morph.begin(); it != v.morph.end(); ) {
     string name;
-    sjis_to_utf8(m.name, name, m.name_len);
+    sjis_to_utf8(it->name, name, it->name_len);
     try {
       string newname = rename_map.at(name);
-      utf8_to_sjis(newname, m.name, m.name_len);
+      if (newname.length() == 0) {
+        it = v.morph.erase(it);
+      } else {
+        utf8_to_sjis(newname, it->name, it->name_len);
+        it++;
+      }
     } catch (std::out_of_range& e) {
+      it++;
       continue;
     }
   }
@@ -46,13 +52,19 @@ void rename_morph(VMD& v, const map<string, string>& rename_map)
 
 void rename_frame(VMD& v, const map<string, string>& rename_map)
 {
-  for (VMD_Frame& f : v.frame) {
+  for (auto it = v.frame.begin(); it != v.frame.end(); ) {
     string name;
-    sjis_to_utf8(f.bonename, name, f.bonename_len);
+    sjis_to_utf8(it->bonename, name, it->bonename_len);
     try {
       string newname = rename_map.at(name);
-      utf8_to_sjis(newname, f.bonename, f.bonename_len);
+      if (newname.length() == 0) {
+        it = v.frame.erase(it);
+      } else {
+        utf8_to_sjis(newname, it->bonename, it->bonename_len);
+        it++;
+      }
     } catch (std::out_of_range& e) {
+      it++;
       continue;
     }
   }
